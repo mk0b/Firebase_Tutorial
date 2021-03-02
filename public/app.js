b5ed2091085e86e98728f2bcb2c7062aefd2777b
Megan Katherine O'Brien <megankob@gmail.com>
2021-03-01 21:27:35 -0800

    Sign and Sign Out set

--- public/app.js
+++ public/app.js
@@ -15,3 +15,23 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+//listening to auth changes and controlling what the user sees when they are signed in/signed out.
+//using data from the user object that is stored when we logged in with google account
+auth.onAuthStateChanged(user => {
+    //seeing what is in user
+    console.log("User Object: ", user);
+    if (user) {
+        //signed in
+        console.log('Logged In');
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        //not signed in
+        console.log('Logged Out');
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
